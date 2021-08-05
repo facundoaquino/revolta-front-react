8eaa06343344a57003de1900af92a0350647a425
Facundo Aquino <facun90@live.com>
2021-08-05 20:09:29 -0300

    load login

--- src/components/formLogin/FormLogin.js
+++ src/components/formLogin/FormLogin.js
@@ -4,34 +4,46 @@ import { UserContext } from '../../context/UserContext'
 import './styles/formLogin.css'
 
 const FormLogin = () => {
+	const [form, setForm] = useState({ name: '', password: '' })
+	const { login } = useContext(UserContext)
+	const [load, setLoad] = useState(false)
+	const handlerSubmit = (e) => {
+		e.preventDefault()
+		setLoad(true)
+		login(form)
+		setLoad(false)
+	}
 
-    const [form, setForm] = useState({ name: '', password: '' })
-    const { login } = useContext(UserContext)
+	const handlerChange = (e) => {
+		setForm({ ...form, [e.target.name]: e.target.value })
 
-    const handlerSubmit = (e) => {
-        e.preventDefault()
-        login(form)
-    }
+		// console.log(form)
+	}
 
-    const handlerChange = (e) => {
-        setForm({ ...form, [e.target.name]: e.target.value })
-
-        // console.log(form)
-
-    }
-
-    return (
-        <div className='form_container'>
-
-            <form onSubmit={handlerSubmit} className='form_form' action="">
-                <label className='form_label' htmlFor="user">Usuario</label>
-                <input onChange={handlerChange} id='user' className='form_input' type="text" name='name' required />
-                <label className='form_label' htmlFor="password">Contraseña</label>
-                <input onChange={handlerChange} id='password' className='form_input' type="password" name='password' required />
-                <button className='form_button'>Enviar</button>
-            </form>
-        </div>
-    )
+	return (
+		<div className="form_container">
+			<form onSubmit={handlerSubmit} className="form_form" action="">
+				<label className="form_label" htmlFor="user">
+					Usuario
+				</label>
+				<input onChange={handlerChange} id="user" className="form_input" type="text" name="name" required />
+				<label className="form_label" htmlFor="password">
+					Contraseña
+				</label>
+				<input
+					onChange={handlerChange}
+					id="password"
+					className="form_input"
+					type="password"
+					name="password"
+					required
+				/>
+				<button disabled={load} className="form_button">
+					Enviar
+				</button>
+			</form>
+		</div>
+	)
 }
 
 export default FormLogin

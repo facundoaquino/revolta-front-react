eb5a119aefc0748120c2646257fedaaad266886a
Facundo Aquino <facun90@live.com>
2021-08-13 17:22:55 -0300

    loader component

--- src/components/HomeVideos/HomeVideos.js
+++ src/components/HomeVideos/HomeVideos.js
@@ -3,20 +3,28 @@ import { Link } from 'react-router-dom'
 import { getRhythms } from '../../helpers/getRhythms'
 import './styles/HomeVideos.css'
 import iconVideo from './../../assets/iconVideo.png'
+import Loader from '../Loader/Loader'
 
 const HomeVideos = () => {
 	const [ritmos, setRitmos] = useState([])
+	const [load, setLoad] = useState(false)
 	// console.log(ritmos)
 
 	useEffect(async () => {
+		setLoad(true)
 		const rhythms = await getRhythms()
 		setRitmos(rhythms)
+		setLoad(false)
 		// console.log(ritmos)
 	}, [])
-	return (
+
+	console.log('render de home videos')
+	return load ? (
+		<Loader />
+	) : (
 		<main className="homeVideos_container">
 			{ritmos.map((ritmo) => (
-				<div className="video_container-link">
+				<div key={ritmo} className="video_container-link">
 					<img className="video_icon" src={iconVideo} alt="" />
 					<Link key={ritmo} className="videos_link" to={`/home/ritmos/${ritmo}`}>
 						{ritmo}
